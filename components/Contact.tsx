c96ffb034e24d420acdeeaa18c1d10ad7eaf00ed
lavanyajagannath <99394963+lavanyajagannath@users.noreply.github.com>
2024-02-29 10:37:13 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,11 +4,11 @@ const Contact = () => {
   return (
     <section id='contact' className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center'>
         <p className='font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide'>
-            04.What's Next?
+            04.What&rsquos Next?
         </p>
         <h2 className='font-titleFont text-5xl font-semibold'>Get In Touch</h2>
         <p className='max-w-[600px] text-center text-textDark'>
-            As I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!
+            As I&rsquom currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&rsquoll try my best to get back to you!
         </p>
         <a href="mailto:lavanya.109998@gmail.com">
             <button className='w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300'>Say Hello</button>
